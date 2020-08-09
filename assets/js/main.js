822e6419f14ae2c8488e582336dce959b3e17d10
pixelicous <pixi@pixelabs.net>
2020-08-09 14:08:33 +0300

    fix refershing issues, minify css

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,16 @@
     setInterval(update_image, 20 * 1000);
     update_image();
 })();
+
+
+// document.addEventListener("DOMContentLoaded",function(){(function(){
+//     var update_image = function () {
+//         var images = document.querySelectorAll('.refresh-images img');
+//         Array.prototype.slice.call(images).forEach(function (img) {
+//             if (img && img.src) {
+//                 img.src = img.src.split('?')[0] + '?_ts=' + (new Date().getTime() / 1000).toFixed(0);
+//             }
+//         });
+//     };
+//     setInterval(update_image, 20 * 1000);
+//     update_image();})();});
